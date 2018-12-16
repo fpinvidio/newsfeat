a942c1068283c00cf50b6f34b908c7a308e0296c
Federico Perez <fperez@cualit.com>
2018-12-16 00:57:59 -0300

    Added like and dislike for recommender

--- htdocs/newsfeat/__init__.py
+++ htdocs/newsfeat/__init__.py
@@ -1,10 +1,7 @@
-import json
-from bson import json_util
-import time
-import metapy
-
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 from searcher import Searcher
+from recommender import Recommender
 from indexer import Indexer
 from flask import Flask
 from flask import render_template
@@ -18,14 +15,51 @@ def index():
 
 @app.route('/search')
 def search():
-    searcher = Searcher("/var/www/cs410.fpinvidio.com/htdocs/newsfeat/htdocs/newsfeat/idx")
-    #searcher = Searcher("idx")
+    #searcher = Searcher("/var/www/cs410.fpinvidio.com/htdocs/newsfeat/htdocs/newsfeat/idx")
+    searcher = Searcher("idx")
     return searcher.search(request.args.get('query'))
+
+@app.route('/recommend')
+def recommend():
+    recommender = Recommender()
+    user_id = int(request.args.get('user_id'))-1
+    return recommender.recommend([user_id])
+
+@app.route('/news/<string:id>/like', methods=['POST'])
+def like_news(id):
+    #client = MongoClient('mongodb://newsfeat:N3usF3at@ds043062.mlab.com:43062/newsfeat')
+    client = MongoClient()
+    db = client.newsfeat
+    news = db.news
+    article = news.find_one({'_id': ObjectId(id)})
+    if article is not None:
+        user_likes = article['user_likes']
+        user_id = int(request.form.get('user_id')) - 1
+        user_likes[user_id] = 1
+        news.update({'_id': ObjectId(id)}, {"$set": {"user_likes": user_likes}})
+        return "{success: true}"
+    return "{success: false}"
+
+@app.route('/news/<string:id>/dislike', methods=['POST'])
+def dislike_news(id):
+    #client = MongoClient('mongodb://newsfeat:N3usF3at@ds043062.mlab.com:43062/newsfeat')
+    client = MongoClient()
+    db = client.newsfeat
+    news = db.news
+    article = news.find_one({'_id': ObjectId(id)})
+    if article is not None:
+        user_likes = article['user_likes']
+        user_id = int(request.form.get('user_id')) - 1
+        user_likes[user_id] = -1
+        news.update({'_id': ObjectId(id)}, {"$set": {"user_likes": user_likes}})
+        return "{success: true}"
+    return "{success: false}"
+
 
 @app.route('/search_index')
 def search_index():
-    indexer = Indexer("data", "/var/www/cs410.fpinvidio.com/htdocs/newsfeat/htdocs/newsfeat/idx")
-    #indexer = Indexer("data", "idx")
+    #indexer = Indexer("data", "/var/www/cs410.fpinvidio.com/htdocs/newsfeat/htdocs/newsfeat/idx")
+    indexer = Indexer("data", "idx")
     return indexer.index()
 
 if __name__ == "__main__":
--- htdocs/newsfeat/recommender.py
+++ htdocs/newsfeat/recommender.py
@@ -1,36 +1,58 @@
-import os
-from whoosh.index import create_in
-from whoosh.fields import Schema, TEXT, ID
-import codecs
+import numpy as np
+from pymongo import MongoClient
+from lightfm import LightFM
+from scipy.sparse import coo_matrix
+import json
+from bson import json_util
 
 class Recommender:
     """
     Wraps the MeTA search engine and its rankers.
     """
-    def __init__(self, root, idx_path):
+    def __init__(self):
         """
         Create/load a MeTA inverted index based on the provided config file and
         set the default ranking algorithm to Okapi BM25.
         """
-        self.root = root
-        self.idx_path = idx_path
+        # client = MongoClient('mongodb://newsfeat:N3usF3at@ds043062.mlab.com:43062/newsfeat')
+        client = MongoClient()
+        db = client.newsfeat
+        news = db.news
+        # news.update_many({}, {"$set" : {"user_likes": [0, 0, 0, 0, 0]}})
+        train = list(news.find({}, {'user_likes': 1, '_id': 0}))
 
-    def recommend(self):
+        def extract_likes(col):
+            return col['user_likes']
 
-        schema = Schema(title=TEXT(stored=True), path=ID(stored=True), \
-                        content=TEXT, textdata=TEXT(stored=True))
-        if not os.path.exists(self.idx_path):
-            os.mkdir(self.idx_path)
+        def extract_ids(col):
+            return col['_id']
 
-        # Creating a index writer to add document as per schema
-        ix = create_in(self.idx_path, schema)
-        writer = ix.writer()
+        train = map(extract_likes, train)
+        train = map(list, zip(*train))
+        train = coo_matrix(train)
+        model = LightFM(loss='warp')
+        model.fit(train, epochs=20)
 
-        filepaths = [os.path.join(self.root, i) for i in os.listdir(self.root)]
-        for path in filepaths:
-            with codecs.open(path, "r", "utf-8") as f:
-                content = f.read()
-                writer.add_document(title=unicode(path.split("/")[1]), path=unicode(path.split("/")[0]), \
-                                    content=content, textdata=content)
-        writer.commit()
-        return "true"
+        items = list(news.find({}, {'_id': 1}))
+        items = np.array(map(extract_ids, items))
+        self.model = model
+        self.items = items
+        self.train = train
+
+    def recommend(self, user_ids):
+        client = MongoClient()
+        db = client.newsfeat
+        news = db.news
+        # number of users and news in training data
+        n_users, n_items = self.train.shape
+        # generate recommendations for each user we input
+        response = {'results': []}
+        for user_id in user_ids:
+            # news our model predicts they will like
+            scores = self.model.predict(user_id, np.arange(n_items))
+            # rank them in order of most liked to least
+            top_items = list(self.items[np.argsort(-scores)])
+            top_items = top_items[:10]
+            top_news = list(news.find({'_id': {'$in': list(top_items)}}))
+            response['results'] = top_news
+        return json.dumps(response, default=json_util.default)
