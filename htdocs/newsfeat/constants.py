a8d0fec9b9cbfbf3e892e3cb7bab1555a6db0ab2
Federico Perez <fperez@cualit.com>
2018-12-16 17:14:31 -0300

    Updated constants

--- htdocs/newsfeat/constants.py
+++ htdocs/newsfeat/constants.py
@@ -1,3 +1,3 @@
-MONGODB_CLIENT='mongodb://newsfeat:N3usF3at@ds043062.mlab.com:43062/newsfeat'
-INDEX_PATH="/var/www/cs410.fpinvidio.com/htdocs/newsfeat/htdocs/newsfeat/idx"
+MONGODB_CLIENT=None
+INDEX_PATH="idx"
 CORPUS_NAME="data"
